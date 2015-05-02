42642a3cf2e4e4c83ca6f1ed198de4069e290eab
podyankee <you@example.com>
2015-05-02 14:17:16 +0300

    Header

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-
-	var wheight = $(window).height();
-
-	$('header').css('height', wheight);
-
-	$(window).resize(function() {
-		var wheight = $(window).height();
-		$('header').css('height', wheight);
-	})
-	
-	});   
 
 
+
+$(".top_phone .wrapper .tab").click(function() {
+	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+
+$(".tabs_header .wrapper .tab").click(function() {
+	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+ 
 
