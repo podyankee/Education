0717ee5aacabb4101b2e3d038f85df2800c2e911
podyankee <you@example.com>
2015-05-04 02:05:56 +0300

    responses added

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+	
+	$.stellar({
+		responsive: true,
+		horizontalOffset:60
+	});
+	
+	$(".carousel").owlCarousel({
+		loop: true ,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true
+			}
+		},
+		navText: ""
+	});
+	
+	
+});
 
 
 
